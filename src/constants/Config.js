b812151bcfd201c17e22299c02762320429cfece
Kumar kad <64307080+Kumarkad@users.noreply.github.com>
2026-01-22 00:43:04 +0530

    Update Config.js

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    API_URL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+    API_URL: import.meta.env.VITE_API_URL || 'https://househelp-1sq5.onrender.com',
 };
 
 export default CONFIG;
